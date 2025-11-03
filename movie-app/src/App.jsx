997153fe515fd762e3b669cec03ca6f428deda2a
Mohamed Abohend <mhmdabohend@gmail.com>
2025-11-03 18:53:32 +0200

    set up routing, Navbar and css styling

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import Home from './pages/Home'
-import './App.css'
-function App() {
-  const [count, setCount] = useState(0)
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import { Routes, Route } from "react-router-dom";
+import "./css/App.css";
+import Navbar from "./components/Navbar";
 
-  return (
-    <>
-      <Home/>
-    </>
-  )
+function App() {
+    return (
+        <>
+            <Navbar />
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/favorites" element={<Favorites />} />
+                </Routes>
+            </main>
+        </>
+    );
 }
 
-export default App
+export default App;
