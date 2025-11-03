351f39a32496ddb3bf996586b982018b8364ffc5
Mohamed Abohend <mhmdabohend@gmail.com>
2025-11-03 18:00:29 +0200

    setup the home page starter view

---
+++ movie-app/src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const movies = [
+        { id: 1, title: "John", release_date: "2024" },
+        { id: 2, title: "Fractions", release_date: "2022" },
+        { id: 3, title: "Weird Things", release_date: "2020" },
+    ];
+
+    return (
+        <div className="home">
+            <form className="search-form">
+                <input
+                    type="text"
+                    className="search-input"
+                    placeholder="Search for a movie"
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">
+                    Search
+                </button>
+            </form>
+            <div className="movies-grid">
+                {movies.map(
+                    (movie) =>
+                        movie.title.toLowerCase().startsWith(searchQuery.toLowerCase()) && (
+                            <MovieCard movie={movie} key={movie.id} />
+                        )
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Home;
